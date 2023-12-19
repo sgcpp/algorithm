97563517107046c6a693d10e62b6b6ece43f79cb
sohug <sohug.ghosh@gmail.com>
2023-12-18 23:40:16 -0500

    add sorting alorithm

---
+++ src/com/java/sort/Sort.java
@@ -0,0 +1,19 @@
+package com.java.sort;
+
+public class Sort {
+    public static void main(String[] args){
+        System.out.println("Hi");
+    }
+    public static int[] bubbleSort(int[] nums){
+
+        return nums;
+    }
+    public static int[] selectionSort(int[] nums){
+
+        return nums;
+    }
+    public static int[] insertionSort(int[] nums){
+
+        return nums;
+    }
+}
